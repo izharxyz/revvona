8126ab65d26ac2e903fb94515e8164cb2d1c6f2e
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-05 01:57:15 +0530

    Add accounts app to INSTALLED_APPS and include its URLs in agavi/urls.py

--- agavi/urls.py
+++ agavi/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/account/', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
