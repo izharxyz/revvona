0a37da8bf42587e49bfee45859937524b841d5da
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-08 01:31:15 +0530

    feat: Update admin.py to register Product and Category models with custom admin options

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'featured', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('featured', 'created_at', 'updated_at')
+
+# Register your models here
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
