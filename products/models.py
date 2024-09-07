d37a3f15fe31ceacf50e7af295fb52e458dd14a8
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-07 10:05:15 +0530

    feat: Add category and product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # This ensures that this model is used only as a base class and won't create its own table
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(TimestampedModel):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField(default=1)
+    image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
