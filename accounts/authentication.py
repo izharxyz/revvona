972f0cf4b6af97aa9789110e98c260e0bf9902c7
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-07 10:39:15 +0530

    fix: Handle token validation failure in CustomJWTAuthentication

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,19 +1,23 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.conf import settings
-
+from rest_framework.exceptions import AuthenticationFailed
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         access_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
-        refresh_token = request.COOKIES.get(
-            settings.SIMPLE_JWT['REFRESH_COOKIE'])
+        refresh_token = request.COOKIES.get(settings.SIMPLE_JWT['REFRESH_COOKIE'])
 
         if access_token:
-            validated_token = self.get_validated_token(access_token)
-            return self.get_user(validated_token), validated_token
+            try:
+                validated_token = self.get_validated_token(access_token)
+                user = self.get_user(validated_token)
+                return user, validated_token
+            except AuthenticationFailed:
+                # Handle token validation failure (optional logging can be added here)
+                return None
 
         if refresh_token:
-            # Optionally handle refresh token logic if needed
+            # Handle refresh token logic if necessary
             pass
 
         return None
