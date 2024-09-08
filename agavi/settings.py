a42f6c6b4671e24761b5f3b20e9ef997742ed981
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-09 02:18:15 +0530

    chore: Add products app to installed apps and URLs

--- agavi/settings.py
+++ agavi/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'accounts',
+    'products',
 ]
 
 MIDDLEWARE = [
--- agavi/urls.py
+++ agavi/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('accounts.urls')),
+    path('api/v1/products/', include('products.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
