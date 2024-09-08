c71013003cb5826e984e68ec17eb7810d8636734
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-09 02:13:15 +0530

    feat: Add URLs for category and product views

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import ProductListView, ProductDetailView, CategoryListView, FeaturedCategoryListView, ProductByCategoryView
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='products'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
+    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('categories/featured/', FeaturedCategoryListView.as_view(), name='featured-categories'),
+    path('categories/<slug:category_slug>/', ProductByCategoryView.as_view(), name='products-by-category'),
+]
