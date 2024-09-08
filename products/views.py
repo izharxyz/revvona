b52e030730e295b6efe1b0ce7c0a41445e177e75
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-09 01:53:15 +0530

    feat: Add category views

--- products/views.py
+++ products/views.py
@@ -29,3 +29,38 @@ class ProductDetailView(generics.RetrieveAPIView):
         except Product.DoesNotExist:
             return Response({"detail": "Product not found."}, status=status.HTTP_404_NOT_FOUND)
         
+        
+# List all categories
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]  # Unauthenticated users can access
+
+    def get(self, request, *args, **kwargs):
+        if not self.queryset.exists():
+            return Response({"detail": "No categories found."}, status=status.HTTP_404_NOT_FOUND)
+        return super().get(request, *args, **kwargs)
+
+
+# List featured categories
+class FeaturedCategoryListView(generics.ListAPIView):
+    queryset = Category.objects.filter(featured=True)
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]  # Unauthenticated users can access
+
+    def get(self, request, *args, **kwargs):
+        if not self.queryset.exists():
+            return Response({"detail": "No featured categories found."}, status=status.HTTP_404_NOT_FOUND)
+        return super().get(request, *args, **kwargs)
+
+# List products by category
+class ProductByCategoryView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]  # Unauthenticated users can access
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        queryset = Product.objects.filter(category__slug=category_slug)
+        if not queryset.exists():
+            raise NotFound(detail="Invalid category or no products found in this category.", code=status.HTTP_404_NOT_FOUND)
+        return queryset
