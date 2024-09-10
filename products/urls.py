e69a7987e12b72694070e83687f062b6c92e0496
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-11 01:19:15 +0530

    refactor: Update URLs for product views

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView, CategoryListView, FeaturedCategoryListView, ProductByCategoryView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='products'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('categories/featured/', FeaturedCategoryListView.as_view(), name='featured-categories'),
