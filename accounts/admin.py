2bac8d41624eb85b584c69155620f2070280b862
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-15 02:46:17 +0530

    refactor: Update AddressAdmin class in accounts/admin.py to improve the display and filtering of address records.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+
 from .models import Address
 
 
-class AddressModelAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "user", "phone_number",
-                    "pin_code", "street", "landmark", "city", "state")
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'phone_number', 'pin_code',
+                    'city', 'state', 'created_at', 'updated_at')
+    search_fields = ('name', 'user__username', 'phone_number',
+                     'pin_code', 'city', 'state')
+    list_filter = ('name', 'phone_number', 'city', 'state', 'updated_at')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
 
-admin.site.register(Address, AddressModelAdmin)
+
+admin.site.register(Address, AddressAdmin)
