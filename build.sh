c910baa5c1c5ddfac6abfa9c1a18711baa1d4746
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-23 03:09:17 +0530

    build: update python versions in build file

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
-python3.9 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
