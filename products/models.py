dc8f6ec037dbb84403383bd4c5f5c5da0d64e8a2
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-25 00:09:17 +0530

    feat: Add quote field to Category model and discount field to Product model

--- products/models.py
+++ products/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(null=True, blank=True)
+    quote = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(upload_to='categories/', null=True, blank=True)
     featured = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True) 
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
@@ -18,12 +20,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount = models.IntegerField(blank=True, null=True)
     stock = models.IntegerField(default=1)
-    image = models.ImageField(null=True, blank=True)
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.SET_NULL, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True) 
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
--- products/serializers.py
+++ products/serializers.py
@@ -1,18 +1,23 @@
 from rest_framework import serializers
+
 from .models import Category, Product
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'image', 'featured', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'description', 'quote',
+                  'image', 'featured', 'created_at', 'updated_at']
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)  # Nested serializer for category
+    # Nested serializer for category
+    category = CategorySerializer(read_only=True)
     category_id = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all(), source='category', write_only=True
     )  # For writing the category via ID
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'stock', 'image', 'category', 'category_id', 'created_at', 'updated_at']
-
+        fields = ['id', 'name', 'description', 'price', 'discount', 'stock',
+                  'image', 'category', 'category_id', 'created_at', 'updated_at']
