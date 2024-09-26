94ebf8a83410737f106f520c0efdd376cb86ba52
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-27 02:44:17 +0530

    feat: Add Review serializer for rating and reviews functionality

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category, Product
+from .models import Category, Product, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -10,14 +10,32 @@ class CategorySerializer(serializers.ModelSerializer):
                   'image', 'featured', 'created_at', 'updated_at']
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()  # Display username instead of ID
+
+    class Meta:
+        model = Review
+        fields = ['id', 'user', 'rating',
+                  'comment', 'created_at', 'updated_at']
+
+
 class ProductSerializer(serializers.ModelSerializer):
-    # Nested serializer for category
     category = CategorySerializer(read_only=True)
     category_id = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all(), source='category', write_only=True
-    )  # For writing the category via ID
+    )
+    average_rating = serializers.DecimalField(
+        max_digits=2, decimal_places=1, read_only=True
+    )
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'discount', 'stock',
-                  'image', 'category', 'category_id', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'description', 'price', 'discount', 'stock', 'image',
+                  'category', 'category_id', 'average_rating', 'created_at', 'updated_at']
+
+
+class ProductDetailSerializer(ProductSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta(ProductSerializer.Meta):
+        fields = ProductSerializer.Meta.fields + ['reviews']
