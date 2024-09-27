efd393714b0157c19e7896a692260e12b15d7ce4
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-28 01:56:14 +0530

    feat: Add Tailwind CSS configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    darkMode: "class",
+    content: ["./dashboard/**/*.{html,py,js}"],
+    colors: {
+        font: {
+            "subtle-light": "rgb(var(--color-font-subtle-light))",
+            "subtle-dark": "rgb(var(--color-font-subtle-dark))",
+            "default-light": "rgb(var(--color-font-default-light))",
+            "default-dark": "rgb(var(--color-font-default-dark))",
+            "important-light": "rgb(var(--color-font-important-light))",
+            "important-dark": "rgb(var(--color-font-important-dark))",
+        },
+        primary: {
+            50: "rgb(var(--color-primary-50))",
+            100: "rgb(var(--color-primary-100))",
+            200: "rgb(var(--color-primary-200))",
+            300: "rgb(var(--color-primary-300))",
+            400: "rgb(var(--color-primary-400))",
+            500: "rgb(var(--color-primary-500))",
+            600: "rgb(var(--color-primary-600))",
+            700: "rgb(var(--color-primary-700))",
+            800: "rgb(var(--color-primary-800))",
+            900: "rgb(var(--color-primary-900))",
+            950: "rgb(var(--color-primary-950))",
+        },
+    },
+};
