b7f6b097262d690c67136070fa989097bdca68e3
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-29 14:48:04 +0530

    refactor: Update address, product and category model fields to enforce not null rules and added migrations for same

--- products/models.py
+++ products/models.py
@@ -6,10 +6,10 @@ from django.db.models import Avg, Count
 
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(blank=True)
-    quote = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(upload_to='categories/', null=True, blank=True)
+    slug = models.SlugField(unique=True, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    quote = models.CharField(max_length=200, null=False, blank=False)
+    image = models.ImageField(upload_to='categories/', null=False, blank=False)
     featured = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)
@@ -21,17 +21,17 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
-    description = models.TextField(blank=True)
+    description = models.TextField(null=False, blank=False)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     discount = models.PositiveIntegerField(blank=True, null=True, validators=[
                                            MinValueValidator(0), MaxValueValidator(100)])
     stock = models.IntegerField(default=1)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to='products/', null=False, blank=False)
     category = models.ForeignKey(
         Category, related_name='products', on_delete=models.SET_NULL, null=True)
 
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
