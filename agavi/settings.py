33fe557735e6ef31c521168da0eaf2e899a942d1
Mohamed Izhar <mohamed@izhar.xyz>
2024-09-29 22:26:44 +0530

    refactor: Update CLOUDINARY_STORAGE settings to enable secure connections

--- agavi/settings.py
+++ agavi/settings.py
@@ -188,6 +188,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
 }
 
 
