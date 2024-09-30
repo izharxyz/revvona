b9726f6b95613e8377e9bcc89ae86cf4e0fdc813
Mohamed Izhar <mohamed@izhar.xyz>
2024-10-01 04:16:18 +0530

    refactor: Move admin url to last for preventing all routes going to admin

--- agavi/urls.py
+++ agavi/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('api/v1/', include('products.urls')),
     path('api/v1/account/', include('accounts.urls')),
     path('api/v1/cart/', include('cart.urls')),
     path('api/v1/checkout/', include('checkout.urls')),
+    path('', admin.site.urls),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
