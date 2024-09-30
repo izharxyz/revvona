2ac49f9862a873d349b6a76c8b4a3ac53372195e
Mohamed Izhar <mohamed@izhar.xyz>
2024-10-01 04:20:55 +0530

    refactor: Update UserSerializer to include addresses

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,36 +1,38 @@
-from .models import Address
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
-    admin = serializers.SerializerMethodField(read_only=True)
-
     class Meta:
         model = User
-        fields = ["id", "username", "email", "admin"]
-
-    def get_admin(self, obj):
-        return obj.is_staff
+        fields = ["id", "username", "email"]
 
 
-# creating tokens manually (with user registration we will also create tokens)
+class ProfileSerializer(UserSerializer):
+    addresses = AddressSerializer(
+        many=True, required=False)
+
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ['addresses']
+
+
+# Creating tokens manually (with user registration we will also create tokens)
 class UserRegisterTokenSerializer(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "admin", "token"]
+        fields = ["id", "username", "email", "token"]
 
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
-
-
-# billing address details
-class AddressSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Address
-        fields = "__all__"
