4a1499667ee521beca64e600f23c223ced6def1e
Mohamed Izhar <mohamed@izhar.xyz>
2024-10-02 21:19:16 +0530

    chore: Update order status choices to include return_initiated and returned option

--- checkout/models.py
+++ checkout/models.py
@@ -10,6 +10,8 @@ class Order(models.Model):
         ('confirmed', 'Confirmed'),
         ('delivered', 'Delivered'),
         ('cancelled', 'Cancelled'),
+        ('return_initiated', 'Return Initiated'),
+        ('returned', 'Returned'),
     ]
 
     user = models.ForeignKey(
