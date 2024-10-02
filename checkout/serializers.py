367a2fcb16b1f24f0b90e237d32370283e55fb3c
Mohamed Izhar <mohamed@izhar.xyz>
2024-10-02 21:20:53 +0530

    Refactor: Update OrderSerializer to include shipping and billing addresses, and item details

--- checkout/serializers.py
+++ checkout/serializers.py
@@ -1,24 +1,28 @@
 from rest_framework import serializers
 
-from accounts.models import Address
 from accounts.serializers import AddressSerializer
+from products.serializers import ProductSerializer
 
 from .models import Order, OrderItem, Payment
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
     class Meta:
         model = OrderItem
         fields = ['id', 'product', 'quantity']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    order_items = OrderItemSerializer(many=True, read_only=True)
+    shipping_address = AddressSerializer(read_only=True)
+    billing_address = AddressSerializer(read_only=True)
+    items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = ['id', 'user', 'shipping_address', 'billing_address',
-                  'order_items', 'total_price', 'status', 'created_at', 'updated_at']
+                  'items', 'total_price', 'status', 'created_at', 'updated_at']
         read_only_fields = ['total_price',
                             'status', 'created_at', 'updated_at']
 
@@ -26,5 +30,5 @@ class OrderSerializer(serializers.ModelSerializer):
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = ['id', 'order', 'method', 'amount', 'razorpay_order_id', 'razorpay_payment_id', 'razorpay_signature',
-                  'payment_status', 'created_at']
+        fields = ['id', 'order', 'method', 'amount', 'razorpay_order_id', 'razorpay_payment_id',
+                  'razorpay_signature', 'payment_status', 'created_at']
