7bf1fe6d6678f79c95ac4aaac97c13aa75eea3cf
Mohamed Izhar <mohamed@izhar.xyz>
2024-10-02 19:04:05 +0530

    Refactor: Add unique constraint to CartItem model and validation so cart can't have multiple same products

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from products.models import Product
@@ -15,10 +16,20 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, related_name='items',
-                             on_delete=models.CASCADE)
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
-        return f"{self.quantity} of {self.product.name} in Cart {self.cart.id}"
+    class Meta:
+        # Ensures the product can't be added multiple times
+        unique_together = ('cart', 'product')
+
+    def save(self, *args, **kwargs):
+        # Check if this product already exists in the cart
+        if CartItem.objects.filter(cart=self.cart, product=self.product).exists() and not self.pk:
+            raise ValidationError(
+                f"The product '{self.product}' is already in the cart.")
+
+        # Proceed with saving if it's not a duplicate
+        super().save(*args, **kwargs)
