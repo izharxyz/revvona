00469a51ddffa88ddd4f0fac0381810cf9c28dae
Mohamed Izhar <mohamed@izhar.xyz>
2024-10-06 01:28:11 +0530

    feat: add email verification and token refresh endpoints as well as change update methods from put to patch

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     # User Registration and Authentication
     path('register/',
          views.UserAuthViewSet.as_view({'post': 'register_user'}), name="register"),
+    path('verify-email/',
+         views.UserAuthViewSet.as_view({'get': 'verify_email'}), name="verify-email"),
     path(
         'login/', views.UserAuthViewSet.as_view({'post': 'login_user'}), name="login"),
+    path('token-refresh/', views.UserAuthViewSet.as_view(
+        {'post': 'refresh_token'}), name="token-refresh"),
     path('logout/',
          views.UserAuthViewSet.as_view({'post': 'logout_user'}), name="logout"),
 
@@ -15,7 +19,7 @@ urlpatterns = [
     path('profile/',
          views.ProfileViewSet.as_view({'get': 'retrieve_user_profile'}), name="user-profile"),
     path('profile/update/',
-         views.ProfileViewSet.as_view({'put': 'update_user_profile'}), name="user-profile-update"),
+         views.ProfileViewSet.as_view({'patch': 'update_user_profile'}), name="user-profile-update"),
     path('profile/delete/',
          views.ProfileViewSet.as_view({'delete': 'delete_user_profile'}), name="user-delete"),
 
@@ -27,7 +31,7 @@ urlpatterns = [
     path('addresses/<int:pk>/',
          views.AddressViewSet.as_view({'get': 'retrieve_user_address'}), name="address-detail"),
     path('addresses/<int:pk>/update/',
-         views.AddressViewSet.as_view({'put': 'update_user_address'}), name="address-update"),
+         views.AddressViewSet.as_view({'patch': 'update_user_address'}), name="address-update"),
     path('addresses/<int:pk>/delete/',
          views.AddressViewSet.as_view({'delete': 'delete_user_address'}), name="address-delete"),
 ]
